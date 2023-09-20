309a91165b97d9f120e5ba7b24959413ccf013ce
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-09-20 20:20:08 +0400

    I modified the greetings.go file, firstly I Imported the Go standard Error Package, secondly I changed the function so that it returns two values, thirdly I added an If statement, and fourthly, I added nil as a second value, you can find more details on the comments where the changes are added inside the greetings.go file

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,23 @@
 package greetings
 
-import "fmt"
+import (
+    // Imported the GO standard errors package 
+    "errors"
+    "fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
+// Updated by changing the function so that it returns two values: a string and an error. My caller will check the second value to see if an error occurred.
+func Hello(name string) (string, error) {
+    // If no name was given, return an error with a message.
+    // Add an if statement to check for an invalid request (an empty string where the name should be) and return an error if the request is invalid. The errors.New function returns an error with my message inside.
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+
+    // If a name was received, return a value that embeds the name
+    // in a greeting message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    // Added nil (meaning no error) as a second value in the successful return. That way, the caller can see that the function succeeded.
+    return message, nil
 }
